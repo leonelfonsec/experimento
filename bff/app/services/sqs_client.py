b2622dbf2d65fe087812f9fdc8a07ff6478f0bc9
Nelson Leonel Fonseca Ochoa <n.fonsecao@uniandes.edu.co>
2025-09-11 16:09:05 -0500

    update bff

--- bff/app/services/sqs_client.py
+++ bff/app/services/sqs_client.py
@@ -1,8 +1,9 @@
 import uuid
 import boto3
+import json
 
 class SQSService:
-    def __init__(self, region_name: str, queue_url: str, default_group_id: str, content_based_dedup: bool):
+    def __init__(self, region_name: str, queue_url: str, default_group_id: str = "grupo1",content_based_dedup: bool = True):
         self.queue_url = queue_url
         self.default_group_id = default_group_id
         self.content_based_dedup = content_based_dedup
@@ -11,11 +12,12 @@ class SQSService:
     def send_message(self, body: str, group_id: str | None = None, dedup_id: str | None = None):
         if not self.queue_url:
             raise ValueError("SQS_QUEUE_URL no está configurado")
+        body_str = json.dumps(body, ensure_ascii=False)
 
         params = {
             "QueueUrl": self.queue_url,
-            "MessageBody": body,
-            "MessageGroupId": group_id or self.default_group_id,  # obligatorio en FIFO
+            "MessageBody": body_str,
+            "MessageGroupId": group_id or self.default_group_id,
         }
 
         # Si NO hay deduplicación por contenido, enviamos un ID único
