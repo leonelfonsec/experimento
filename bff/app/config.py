a6490980f0746f5b741006663ab3ac29df19fd59
Nelson Leonel Fonseca Ochoa <n.fonsecao@uniandes.edu.co>
2025-09-13 11:29:53 -0500

    ok

--- bff/app/config.py
+++ bff/app/config.py
@@ -1,6 +1,8 @@
 import os
 
 class Config:
+    DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://user:password@db:5432/db")
+
     # Región y cola (ajústalo a tu caso)
     AWS_REGION = os.getenv("AWS_REGION", "us-east-2")
     SQS_QUEUE_URL = os.getenv("SQS_QUEUE_URL")  # p.ej. https://sqs.us-east-2.amazonaws.com/4117.../prueba-cola.fifo
